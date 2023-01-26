eceaea73746a4c6bef3db6354577687edbc9176c
B-Karen <heberlisfinol@gmail.com>
2023-01-26 18:47:29 -0300

    empezando md links

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+
+const mdLinks = (path, options) => {
+  return new Promise((resolve, reject) => {
+    // Identificar si la ruta existe
+    if (fs.existsSync(path)) {
+// Chequear o convertir a ruta absoluta
+    } else {
+      // Si no existe la ruta se rechaza
+      reject('La ruta no existe');
+    };
+
+  });
+};
+module.exports = {
+  mdLinks
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,4 @@
-const mdLinks = require('../');
+const{ mdLinks} = require('../index.js');
 
 
 describe('mdLinks', () => {
@@ -6,5 +6,16 @@ describe('mdLinks', () => {
   it('should...', () => {
     console.log('FIX ME!');
   });
+
+  // it('Deberia devolver una promesa', () => {
+  //   expect(mdLinks()).toBe(typeof Promise);
+  // });
+  it( 'Debe rechazar cuando el path no existe',()=> {
+    return(mdLinks('/karen/biblioteca.mdLinks').catch((error)=>{
+      expect(error).toBe('La ruta no existe');
+
+    }))
+  });
+
 
 });
